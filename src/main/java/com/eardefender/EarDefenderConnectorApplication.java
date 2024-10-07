33ed697a56530ea2e306aa883279c2f28c9db4a4
Łukasz Jałocha <266533@student.pwr.edu.pl>
2024-10-07 22:26:41 +0200

    Add basic project structure

---
+++ src/main/java/com/eardefender/EarDefenderConnectorApplication.java
@@ -0,0 +1,13 @@
+package com.eardefender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EarDefenderConnectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EarDefenderConnectorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eardefender/EarDefenderConnectorTests.java
@@ -0,0 +1,13 @@
+package com.eardefender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EarDefenderConnectorTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
