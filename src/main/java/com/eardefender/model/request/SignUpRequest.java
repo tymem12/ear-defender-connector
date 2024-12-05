804990a1addcce26bac8a717d194a368ecd5fd71
Łukasz Jałocha <266533@student.pwr.edu.pl>
2024-12-05 22:01:21 +0100

    Fix sign up request and log in request

--- src/main/java/com/eardefender/model/request/SignUpRequest.java
+++ src/main/java/com/eardefender/model/request/SignUpRequest.java
@@ -2,14 +2,18 @@ package com.eardefender.model.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 @EqualsAndHashCode(callSuper = true)
 @Schema(description = "Request Model for logging in and signing up")
 @SuperBuilder
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class SignUpRequest extends CredentialsRequest {
 
     @Schema(description = "User's full name",
